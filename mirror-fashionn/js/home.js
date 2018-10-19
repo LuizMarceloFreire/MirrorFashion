11f49d6b39da91b3b1b358dee0d8ddeb3d368b7b
Luiz Marcelo <luizmarcelosenac@gmail.com>
2018-10-19 08:57:53 -0300

    Subindo Site

---
+++ mirror-fashionn/js/home.js
@@ -0,0 +1,8 @@
+$('.novidades button').click(function() {
+$('.novidades').addClass('painel-aberto');
+});
+
+$('.novidades button').click(function() {
+$('.novidades').toggleClass('painel-aberto');
+});
+
